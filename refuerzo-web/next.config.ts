e980836e664668f77a72e768485be6341364d907
wdavid <00135222@uca.edu.sv>
2025-01-30 23:32:34 +0100

    dashboard

--- refuerzo-web/next.config.ts
+++ refuerzo-web/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['scontent.fsvq6-1.fna.fbcdn.net', 'via.placeholder.com'],
+  },
 };
 
 export default nextConfig;
