26b0e2195c1f525bc871daf2a04a996f36b252fe
JosseJuventino <00048322@uca.edu.sv>
2025-02-07 22:02:50 -0600

    fix: repeat layout

--- refuerzo-web/components/ClientProvider.tsx
+++ refuerzo-web/components/ClientProvider.tsx
@@ -1,9 +1,9 @@
-'use client';
+"use client"
 
 import { ReactNode } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-// Creas el queryClient dentro de un Client Component
+
 const queryClient = new QueryClient();
 
 export default function ClientProviders({ children }: { children: ReactNode }) {
