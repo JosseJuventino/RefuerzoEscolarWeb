62be77cf90cb8e29832f75bf6e2aa609bb501c8c
JosseJuventino <00048322@uca.edu.sv>
2025-02-13 21:45:50 -0600

    Add Geolocation service

--- refuerzo-apiv2/src/auth/auth.controller.ts
+++ refuerzo-apiv2/src/auth/auth.controller.ts
@@ -7,14 +7,14 @@ import {
   Param,
   Delete,
   UseGuards,
-  Request,
+  Req,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
 import { PageverifyDto } from './dto/pageverify.dto';
 import { ApiBasicAuth, ApiBearerAuth } from '@nestjs/swagger';
 import { Public } from 'src/common/decorators/public.decorators';
-
+import { Request } from 'express';
 @Controller('auth')
 @ApiBasicAuth()
 export class AuthController {
@@ -22,7 +22,9 @@ export class AuthController {
 
   @Post('login')
   @Public()
-  login(@Body() authDto: AuthDto) {
-    return this.authService.login(authDto);
+  login(@Body() authDto: AuthDto, @Req() req: Request) {
+    const ipAddress = req.ip || req.connection.remoteAddress; // Obtener la dirección IP
+    const userAgent = req.headers['user-agent'] || 'Unknown'; // Obtener el User-Agent
+    return this.authService.login(authDto, ipAddress, userAgent);
   }
 }
--- refuerzo-apiv2/src/auth/auth.service.ts
+++ refuerzo-apiv2/src/auth/auth.service.ts
@@ -13,6 +13,10 @@ import { Tokens } from './entities/token.entity';
 import { CreateTokensDto } from './dto/create-tokens.dto';
 import { PageverifyDto } from './dto/pageverify.dto';
 import { PermisionOptions, Role } from 'src/roles/entities/role.entity';
+import { LoginAudit } from './entities/loginAudith.entity';
+import { GeoLocationService } from './geolocation.service';
+import { UAParser } from 'ua-parser-js';
+import { ObjectId } from 'typeorm';
 
 @Injectable()
 export class AuthService {
@@ -20,6 +24,9 @@ export class AuthService {
   private readonly authcrudHelper: CrudHelper<Tokens>;
   private readonly rolecrudHelper: CrudHelper<Role>;
   constructor(
+    @InjectRepository(LoginAudit)
+    private readonly loginAuditRepository: Repository<LoginAudit>,
+    private readonly geoLocationService: GeoLocationService,
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
     @InjectRepository(Role)
@@ -36,7 +43,7 @@ export class AuthService {
     this.rolecrudHelper = new CrudHelper<Role>(this.roleRepository, 'Roles');
   }
 
-  async login(authDto: AuthDto): Promise<GeneralResponseDto<auth>> {
+  async login(authDto: AuthDto, ipAddress: string, userAgent: string): Promise<GeneralResponseDto<auth>> {
     const user = await this.UsercrudHelper.findOne(
       {
         where: { email: authDto.email },
@@ -117,6 +124,24 @@ export class AuthService {
       {},
     );
 
+    const location = this.geoLocationService.getLocation(ipAddress);
+    const parser = new UAParser(userAgent);
+    const result = parser.getResult();
+    const device = result.device.type || 'Desktop'; // Móvil, Tablet, etc.
+    const browser = result.browser.name || 'Unknown'; // Navegador
+
+    await this.logLoginAttempt(
+      user._id.toString(),
+      user.email,
+      ipAddress,
+      userAgent,
+      device,
+      browser,
+      location.country,
+      location.region,
+    );
+
+
     return (
       new GeneralResponseBuilder<auth>()
         .setStatusCode(200)
@@ -126,4 +151,43 @@ export class AuthService {
         .build()
     );
   }
+
+  private async logLoginAttempt(
+    userId: string,
+    email: string,
+    ipAddress: string,
+    userAgent: string,
+    device: string,
+    browser: string,
+    country: string,
+    region: string,
+  ): Promise<void> {
+    const newLog = this.loginAuditRepository.create({
+      userId,
+      email,
+      ipAddress,
+      userAgent,
+      device,
+      browser,
+      country,
+      region,
+    });
+
+    await this.loginAuditRepository.save(newLog);
+
+    const userLogs = await this.loginAuditRepository.find({
+      where: { userId },
+      order: { createdAt: 'DESC' },
+    });
+
+    if (userLogs.length > 3) {
+      const logsToDelete = userLogs.slice(3);
+      for (const log of logsToDelete) {
+        log.expired = true;
+        await this.loginAuditRepository.save(log);
+      }
+    }
+  }
+
+
 }
